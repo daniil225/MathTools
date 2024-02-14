18191af8f6b6b107bbb10cdc8b0bfe266f0fc08e
daniil225 <31102560+daniil225@users.noreply.github.com>
2024-02-14 05:34:01 +0200

    reformate_folder_struct

--- лаба1/FDM.h
+++ лаба1/FDM.h
@@ -56,7 +56,7 @@ private:
     
     /* Генерация Матрицы в которой уже учитывается все необходимое тоесть все КУ */
     bool GenerateMatrix();
-    const int coef = 8;
+    const int coef = 1;
 public: 
 
     FDM() = delete;
